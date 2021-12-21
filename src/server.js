f869240cfac839a086ee55755f6a3ad44af65b04
Weenix Star <60514231+weenix1@users.noreply.github.com>
2021-12-21 14:06:26 +0100

    commit

--- src/server.js
+++ src/server.js
@@ -2,13 +2,21 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import authorsRouter from "./services/Authors/index.js";
 import booksRouter from "./services/Books/index.js";
+import cors from "cors";
 
 const server = express();
 
 server.use(express.json());
-
+server.use(cors());
 server.use("/Books", booksRouter);
 server.use("/Authors", authorsRouter);
+
+// *********************** ERROR MIDDLEWARES ***************************
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 const port = 3001;
 console.table(listEndpoints(server));
